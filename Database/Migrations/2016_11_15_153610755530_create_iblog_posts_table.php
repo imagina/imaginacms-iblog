d1b66fee5f59f7fc2a6e64128d81b24f5b960abb
michael <38920570+msolanoimagina@users.noreply.github.com>
2019-07-29 14:41:10 -0500

    +Add: seeder to create Roles: Editor and Author

--- Database/Migrations/2016_11_15_153610755530_create_iblog_posts_table.php
+++ Database/Migrations/2016_11_15_153610755530_create_iblog_posts_table.php
@@ -5,33 +5,35 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateIblogPostsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('iblog__posts', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id')->unsigned();
-            // fields
-            $table->text('options')->default('')->nullable();
-            $table->integer('status')->default(0)->unsigned();
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on(config('auth.table', 'users'))->onDelete('restrict');
-            // Your fields
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('iblog__posts', function (Blueprint $table) {
+      $table->engine = 'InnoDB';
+      $table->increments('id')->unsigned();
+      // fields
+      $table->text('options')->default('')->nullable();
+      $table->integer('status')->default(0)->unsigned();
+      $table->integer('category_id')->unsigned();
+      $table->foreign('category_id')->references('id')->on('iblog__categories')->onDelete('restrict');
+      $table->integer('user_id')->unsigned();
+      $table->foreign('user_id')->references('id')->on(config('auth.table', 'users'))->onDelete('restrict');
+      // Your fields
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('iblog__posts');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('iblog__posts');
+  }
 }
