51ab0ff9bc3cfcb03e68f332e1c72b4a049b6fb0
Marcos Rativa <marcosr@imaginacolombia.com>
2020-03-10 10:51:09 -0500

    Preparing Version 4.0.23

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -1,25 +1,28 @@
 <?php
 
 use Illuminate\Routing\Router;
-$categoryRepository= app('Modules\Iblog\Repositories\CategoryRepository');
-$locale = LaravelLocalization::setLocale() ?: App::getLocale();
-$categories=$categoryRepository->getItemsBy(json_decode(json_encode(['filter'=>[],'include'=>[],'take'=>null])));
-foreach ($categories as  $category){
 
-    /** @var Router $router */
-    $router->group(['prefix' => $category->slug], function (Router $router)use($locale,$category) {
+if (!App::runningInConsole()) {
+    $categoryRepository = app('Modules\Iblog\Repositories\CategoryRepository');
+    $locale = LaravelLocalization::setLocale() ?: App::getLocale();
+    $categories = $categoryRepository->getItemsBy(json_decode(json_encode(['filter' => [], 'include' => [], 'take' => null])));
+    foreach ($categories as $category) {
 
-        $router->get('/', [
-            'as' => $locale . '.iblog.category.'.$category->slug,
-            'uses' => 'PublicController@index',
-            'middleware' => config('asgard.iblog.config.middleware'),
-        ]);
-        $router->get('{slug}', [
-            'as' => $locale . '.iblog.'.$category->slug.'.post',
-            'uses' => 'PublicController@show',
-            'middleware' => config('asgard.iblog.config.middleware'),
-        ]);
-    });
+        /** @var Router $router */
+        $router->group(['prefix' => $category->slug], function (Router $router) use ($locale, $category) {
+
+            $router->get('/', [
+                'as' => $locale . '.iblog.category.' . $category->slug,
+                'uses' => 'PublicController@index',
+                'middleware' => config('asgard.iblog.config.middleware'),
+            ]);
+            $router->get('{slug}', [
+                'as' => $locale . '.iblog.' . $category->slug . '.post',
+                'uses' => 'PublicController@show',
+                'middleware' => config('asgard.iblog.config.middleware'),
+            ]);
+        });
+    }
 }
 /** @var Router $router */
 $router->group(['prefix' => trans('iblog::tag.uri')], function (Router $router) use ($locale) {
@@ -32,7 +35,7 @@ $router->group(['prefix' => trans('iblog::tag.uri')], function (Router $router) 
 
 
 /** @var Router $router */
-$router->group(['prefix' => 'iblog/feed'], function (Router $router) use($locale){
+$router->group(['prefix' => 'iblog/feed'], function (Router $router) use ($locale) {
     $router->get('{format}', [
         'as' => $locale . '.iblog.feed.format',
         'uses' => 'PublicController@feed',
