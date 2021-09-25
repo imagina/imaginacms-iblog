ba15feacabfa2a8fe9c51451a5a91aace11aa268
Juan Escalante <jcec007@gmail.com>
2021-09-25 12:54:03 -0500

    +Update: Cache Decorator Files

--- Repositories/Cache/CacheCategoryDecorator.php
+++ Repositories/Cache/CacheCategoryDecorator.php
@@ -7,25 +7,60 @@ use Modules\Core\Repositories\Cache\BaseCacheDecorator;
 
 class CacheCategoryDecorator extends BaseCacheDecorator implements CategoryRepository
 {
-    public function __construct(CategoryRepository $category)
-    {
-        parent::__construct();
-        $this->entityName = 'categories';
-        $this->repository = $category;
-    }
-
-    public function getItemsBy($params)
-    {
-        return $this->remember(function () use ($params) {
-            return $this->repository->getItemsBy($params);
-        });
-    }
-
-    public function getItem($criteria, $params)
-    {
-        return $this->remember(function () use ($criteria, $params) {
-            return $this->repository->getItem($criteria, $params);
-        });
-    }
-
+  public function __construct(CategoryRepository $category)
+  {
+    parent::__construct();
+    $this->entityName = 'categories';
+    $this->repository = $category;
+  }
+  
+  public function getItemsBy($params)
+  {
+    return $this->remember(function () use ($params) {
+      return $this->repository->getItemsBy($params);
+    });
+  }
+  
+  public function getItem($criteria, $params)
+  {
+    return $this->remember(function () use ($criteria, $params) {
+      return $this->repository->getItem($criteria, $params);
+    });
+  }
+  
+  /**
+   * create a resource
+   *
+   * @return mixed
+   */
+  public function create($data)
+  {
+    $this->clearCache();
+    return $this->repository->create($data);
+  }
+  
+  /**
+   * update a resource
+   *
+   * @return mixed
+   */
+  public function updateBy($criteria, $data, $params)
+  {
+    $this->clearCache();
+    
+    return $this->repository->updateBy($criteria, $data, $params);
+  }
+  
+  /**
+   * destroy a resource
+   *
+   * @return mixed
+   */
+  public function deleteBy($criteria, $params)
+  {
+    $this->clearCache();
+    
+    return $this->repository->deleteBy($criteria, $params);
+  }
+  
 }
--- Repositories/Cache/CachePostDecorator.php
+++ Repositories/Cache/CachePostDecorator.php
@@ -8,46 +8,83 @@ use Modules\Core\Repositories\Cache\BaseCacheDecorator;
 
 class CachePostDecorator extends BaseCacheDecorator implements PostRepository
 {
-    public function __construct(PostRepository $post)
-    {
-        parent::__construct();
-        $this->entityName = 'posts';
-        $this->repository = $post;
-    }
-
-
-    /**
-     * @param object $id
-     * @return object
-     */
-    public function whereCategory($id)
-    {
-        return $this->remember(function () use ($id) {
-            return $this->repository->whereCategory($id);
-        });
-    }
-
-    /**
-     * @param $params
-     * @return mixed
-     */
-    public function getItemsBy($params)
-    {
-        return $this->remember(function () use ($params) {
-            return $this->repository->getItemsBy($params);
-        });
-    }
-
-    /**
-     * @param $criteria
-     * @param $params
-     * @return mixed
-     */
-    public function getItem($criteria, $params)
-    {
-        return $this->remember(function () use ($criteria, $params) {
-            return $this->repository->getItem($criteria, $params);
-        });
-    }
-
+  public function __construct(PostRepository $post)
+  {
+    parent::__construct();
+    $this->entityName = 'posts';
+    $this->repository = $post;
+  }
+  
+  
+  /**
+   * @param object $id
+   * @return object
+   */
+  public function whereCategory($id)
+  {
+    return $this->remember(function () use ($id) {
+      return $this->repository->whereCategory($id);
+    });
+  }
+  
+  /**
+   * @param $params
+   * @return mixed
+   */
+  public function getItemsBy($params)
+  {
+    return $this->remember(function () use ($params) {
+      return $this->repository->getItemsBy($params);
+    });
+  }
+  
+  /**
+   * @param $criteria
+   * @param $params
+   * @return mixed
+   */
+  public function getItem($criteria, $params)
+  {
+    return $this->remember(function () use ($criteria, $params) {
+      return $this->repository->getItem($criteria, $params);
+    });
+  }
+  
+  /**
+   * create a resource
+   *
+   * @return mixed
+   */
+  public function create($data)
+  {
+    $this->clearCache();
+    return $this->repository->create($data);
+  }
+  
+  /**
+   * destroy a resource
+   *
+   * @return mixed
+   */
+  public function deleteBy($criteria, $params)
+  {
+    $this->clearCache();
+    
+    return $this->repository->deleteBy($criteria, $params);
+  }
+  
+  
+  /**
+   * update a resource
+   *
+   * @return mixed
+   */
+  public function updateBy($criteria, $data, $params)
+  {
+    $this->clearCache();
+    
+    return $this->repository->updateBy($criteria, $data, $params);
+  }
+  
+  
 }
