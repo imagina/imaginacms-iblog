b924c891b728e620caacaa309f749f65a38910e9
Juan Escalante <jcec007@gmail.com>
2021-10-04 12:42:04 -0500

    +Fix: timeline layout 2

--- Resources/views/frontend/components/timeline/layouts/timeline-layout-2/index.blade.php
+++ Resources/views/frontend/components/timeline/layouts/timeline-layout-2/index.blade.php
@@ -1,13 +1,11 @@
 <div id="timelineLayout2" class="timeline">
 
-
-<div class="timeline">
     <div class="entry">
         @foreach($items as $index => $item)
       <div class="title">
         <h3>06 | 05 <br>
         Title <br> 9:00 Am </h3>
-        <i class="{{$item->icom}}" <style  opacity: 0.5;> </style></i>
+        <i class="{{$item->icom}}" style=" opacity: 0.5;"></i>
       </div>
       <div class="body">
         <hr>
