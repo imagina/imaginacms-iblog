f297f09fa6e0908721eb12d2140e735661747797
Michael <msolano@imaginacolombia.com>
2022-02-03 12:36:52 -0500

    Add: CMS Translations

---
+++ Resources/lang/en/cms.php
@@ -0,0 +1,18 @@
+<?php
+return [
+  "newPost"  => 'New Post',
+  "updatePost"  => 'Update Post',
+  "newCategory"  => 'New Category',
+  "updateCategory"  => 'Update Category',
+  "post"  => 'Post',
+  "quickCard"  => [
+    "lastPosts"  => 'Blog - Last Posts'
+  ],
+  "form"  => [
+    "showInMenu" => 'Show in Menu',
+    "featured"  => 'Featured',
+    "sortOrder"  => 'Sort Order',
+    "availableDate"  => 'Available Date'
+
+  ],
+];
---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,18 @@
+<?php
+return [
+  "newPost" => 'Nueva Entrada',
+  "updatePost" => 'Actualizar Entrada',
+  "newCategory" => 'Nueva Categoría',
+  "updateCategory" => 'Actualizar Categoría',
+  "post" => 'Entrada',
+  "quickCard" => [
+    "lastPosts" => 'Blog - Ultimos Posts'
+  ],
+  "form" => [
+    "showInMenu"=>'Mostrar en menu',
+    "featured" => 'Destacado',
+    "sortOrder" => 'Orden de prioridad',
+    "availableDate" => 'Disponible desde'
+
+  ],
+];
