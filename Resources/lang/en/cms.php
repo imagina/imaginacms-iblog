30e774419bd464b10c8a4d40ca1240084d636790
Michael <msolano@imaginacolombia.com>
2022-02-11 19:09:03 -0500

    +Update: cms Translation files

--- Resources/lang/en/cms.php
+++ Resources/lang/en/cms.php
@@ -1,18 +1,25 @@
 <?php
-return [
-  "newPost"  => 'New Post',
-  "updatePost"  => 'Update Post',
-  "newCategory"  => 'New Category',
-  "updateCategory"  => 'Update Category',
-  "post"  => 'Post',
-  "quickCard"  => [
-    "lastPosts"  => 'Blog - Last Posts'
-  ],
-  "form"  => [
-    "showInMenu" => 'Show in Menu',
-    "featured"  => 'Featured',
-    "sortOrder"  => 'Sort Order',
-    "availableDate"  => 'Available Date'
 
+return [
+  "sidebar" => [
+    "adminGroup" => "Blog",
+    "adminPosts" => "Blog Posts",
+    "adminCategories" => "Blog Categories",
+    "adminPostsInternal" => "View Blog Posts"
   ],
+  "newPost" => "New Post",
+  "updatePost" => "Update Post",
+  "newCategory" => "New Category",
+  "updateCategory" => "Update Category",
+  "post" => "Post",
+  "quickCard" => [
+    "lastPosts" => "Blog - Last Posts"
+  ],
+  "form" => [
+    "showInMenu" => "Show in Menu",
+    "featured" => "Featured",
+    "sortOrder" => "Sort Order",
+    "availableDate" => "Available Date"
+  ]
 ];
+
--- Resources/lang/es/cms.php
+++ Resources/lang/es/cms.php
@@ -1,18 +1,24 @@
 <?php
+
 return [
-  "newPost" => 'Nueva Entrada',
-  "updatePost" => 'Actualizar Entrada',
-  "newCategory" => 'Nueva Categoría',
-  "updateCategory" => 'Actualizar Categoría',
-  "post" => 'Entrada',
+  "sidebar" => [
+    "adminGroup" => "Blog",
+    "adminPosts" => "Entradas de Blog",
+    "adminCategories" => "Categorias de Blog",
+    "adminPostsInternal" => "Ver entradas de Blog"
+  ],
+  "newPost" => "Nueva Entrada",
+  "updatePost" => "Actualizar Entrada",
+  "newCategory" => "Nueva Categoría",
+  "updateCategory" => "Actualizar Categoría",
+  "post" => "Entrada",
   "quickCard" => [
-    "lastPosts" => 'Blog - Ultimos Posts'
+    "lastPosts" => "Blog - Ultimos Posts"
   ],
   "form" => [
-    "showInMenu"=>'Mostrar en menu',
-    "featured" => 'Destacado',
-    "sortOrder" => 'Orden de prioridad',
-    "availableDate" => 'Disponible desde'
-
-  ],
+    "showInMenu" => "Mostrar en menu",
+    "featured" => "Destacado",
+    "sortOrder" => "Orden de prioridad",
+    "availableDate" => "Disponible desde"
+  ]
 ];
