7bb1ac311c189249c7f6c0ae160055b2a61c0b9e
T-JULIAND <juliandavidbonillatorres@gmail.com>
2022-07-15 09:10:47 -0500

    +Fix | Front-end : meta Keywords post (Mete_keywords and Tags)

--- Resources/views/frontend/partials/post/metas.blade.php
+++ Resources/views/frontend/partials/post/metas.blade.php
@@ -1,15 +1,18 @@
-<meta name="description" content="{{$post->meta_description ?? strip_tags($post->summary) ?? strip_tags($post->description)}}">
+<meta name="description"
+      content="{{$post->meta_description ?? strip_tags($post->summary) ?? strip_tags($post->description)}}">
 <!-- Schema.org para Google+ -->
 
 <meta itemprop="name" content="{{$post->meta_title ?? $post->title}}">
-<meta itemprop="description" content="{{$post->meta_description ?? strip_tags($post->summary) ?? strip_tags($post->description)}}">
+<meta itemprop="description"
+      content="{{$post->meta_description ?? strip_tags($post->summary) ?? strip_tags($post->description)}}">
 <meta itemprop="image" content=" {{url($post->mainimage->path) }}">
 <!-- Open Graph para Facebook-->
 <meta property="og:title" content="{{$post->meta_title ?? $post->title}}"/>
 <meta property="og:type" content="article"/>
 <meta property="og:url" content="{{$post->url}}"/>
 <meta property="og:image" content="{{url($post->mainimage->path)}}"/>
-<meta property="og:description" content="{{$post->meta_description ?? strip_tags($post->summary) ?? strip_tags($post->description)}}"/>
+<meta property="og:description"
+      content="{{$post->meta_description ?? strip_tags($post->summary) ?? strip_tags($post->description)}}"/>
 <meta property="og:site_name" content="{{Setting::get('core::site-name') }}"/>
 <meta property="og:locale" content="{{config('asgard.iblog.config.oglocale')}}">
 
@@ -17,6 +20,12 @@
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="{{ Setting::get('core::site-name') }}">
 <meta name="twitter:title" content="{{$post->meta_title ?? $post->title}}">
-<meta name="twitter:description" content="{{$post->meta_description ?? strip_tags($post->summary) ?? strip_tags($post->description)}}">
+<meta name="twitter:description"
+      content="{{$post->meta_description ?? strip_tags($post->summary) ?? strip_tags($post->description)}}">
 <meta name="twitter:creator" content="{{Setting::get('iblog::twitter') }}">
 <meta name="twitter:image:src" content="{{url($post->mainimage->path)}}">
+<meta name="keywords" content="{{implode("," ,$post->meta_keywords) ?? ""}},
+@foreach($post->tags as $tag)
+<?php echo $tag->name. "," ?>
+@endforeach
+">
