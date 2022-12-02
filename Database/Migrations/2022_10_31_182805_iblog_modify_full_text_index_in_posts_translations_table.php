fa2d0dde78183dc7c54759d716404d49ce7dd5f4
Cristian Marin <marin101998@gmail.com>
2022-12-02 18:06:46 -0500

    +fix | Change name migration revision

--- Database/Migrations/2022_10_31_182805_iblog_modify_full_text_index_in_posts_translations_table.php
+++ Database/Migrations/2022_10_31_182805_iblog_modify_full_text_index_in_posts_translations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class IblogModifyFullTextIndexInPostsTranslationsTable extends Migration
+class IblogModifyFullTextIndexInPostsTranslationsFixTable extends Migration
 {
     /**
      * Run the migrations.
