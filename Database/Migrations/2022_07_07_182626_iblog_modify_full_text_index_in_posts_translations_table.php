3bb2a8f7ad760d0af29686db477655e2feeda676
Cristian Marin <marin101998@gmail.com>
2022-12-06 15:13:51 -0500

    +fix | delete validation migration indexes

--- Database/Migrations/2022_07_07_182626_iblog_modify_full_text_index_in_posts_translations_table.php
+++ Database/Migrations/2022_07_07_182626_iblog_modify_full_text_index_in_posts_translations_table.php
@@ -6,28 +6,24 @@ use Illuminate\Database\Migrations\Migration;
 
 class IblogModifyFullTextIndexInPostsTranslationsTable extends Migration
 {
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    $sm = Schema::getConnection()->getDoctrineSchemaManager();
-    $indexesFound = $sm->listTableIndexes('iblog__post_translations');
-    if (!array_key_exists("full", $indexesFound)) {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
       \DB::statement("ALTER TABLE `iblog__post_translations` DROP INDEX `full`;");
       \DB::statement("ALTER TABLE iblog__post_translations ADD FULLTEXT full(title,description,summary)");
     }
-  }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    //
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
 }
