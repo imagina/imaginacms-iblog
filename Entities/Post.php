32cbdb99ed64d754e2f91f033392390717a895ee
Juan Escalante <jcec007@gmail.com>
2023-01-05 18:33:57 -0500

    +Fix: post url, change env by config sentence

--- Entities/Post.php
+++ Entities/Post.php
@@ -174,7 +174,7 @@ class Post extends Model implements TaggableInterface
     if (empty($this->slug)) return "";
 
     $currentLocale = locale();
-    $tenantDomain = isset(tenant()->id) ? tenant()->domain : (tenancy()->find($this->organization_id)->domain ?? parse_url(env('APP_URL', 'localhost'),PHP_URL_HOST));
+    $tenantDomain = isset(tenant()->id) ? tenant()->domain : (tenancy()->find($this->organization_id)->domain ?? parse_url(config("app.url"),PHP_URL_HOST));
 
     if (isset($this->options->urlCoder) && !empty($this->options->urlCoder)) {
       if ($this->options->urlCoder == "onlyPost") {
