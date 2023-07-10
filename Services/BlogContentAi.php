c3b6af674febe282a08baab4c13f7ea915d27a82
William Verde <williamverde10@gmail.com>
2023-07-10 12:15:20 -0400

    Update: renamed names to ai

--- Services/BlogContentAi.php
+++ Services/BlogContentAi.php
@@ -4,16 +4,16 @@
 namespace Modules\Iblog\Services;
 
 use Illuminate\Http\Request;
-use Modules\Isite\Services\IAService;
+use Modules\Isite\Services\AiService;
 use Modules\Iblog\Entities\Category;
 
-class BlogContentIA
+class BlogContentAi
 {
   public $iaService;
 
   function __construct()
   {
-    $this->iaService = new IAService();
+    $this->aiService = new AiService();
   }
 
   public function getPosts($quantity = 2)
@@ -21,12 +21,12 @@ class BlogContentIA
     //instance the prompt to generate the posts
     $prompt = "Contenido para post de blog que seran usados en un sitio WEB con los siguientes atributos ";
     //Instance attributes
-    $prompt .= $this->iaService->getStandardPrompts(
+    $prompt .= $this->aiService->getStandardPrompts(
       ["title", "description", "summary", "slug", "category_id"],
       ["categories" => Category::get()]
     );
     //Call IA Service
-    $response = $this->iaService->getContent($prompt, $quantity);
+    $response = $this->aiService->getContent($prompt, $quantity);
     //Return response
     return $response;
   }
