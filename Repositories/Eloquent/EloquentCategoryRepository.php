be2396597ab54a748163c78b8753161457e992e8
William Verde <williamverde10@gmail.com>
2023-12-01 10:14:52 -0400

    Added extra params in method to be compatible with Icrud - Category Repository

--- Repositories/Eloquent/EloquentCategoryRepository.php
+++ Repositories/Eloquent/EloquentCategoryRepository.php
@@ -142,7 +142,7 @@ class EloquentCategoryRepository extends EloquentCrudRepository implements Categ
    * @param $query
    * @param $relations
    */
-  public function includeToQuery($query, $relations)
+  public function includeToQuery($query, $relations, $method = null)
   {
 
     //request all categories instances in the "relations" attribute in the entity model
