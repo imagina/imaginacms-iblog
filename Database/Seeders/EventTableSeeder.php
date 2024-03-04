22ebdf7f80fead3ed4fe3cee9279242f207675c0
james Olaya <jamesmixolaya@gmail.com>
2024-03-04 15:21:50 -0500

    seed_event

---
+++ Database/Seeders/EventTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Modules\Iblog\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Modules\Isite\Entities\event;
+
+
+
+class EventTableSeeder extends Seeder
+{
+    public $events = [
+        "PostWasCreated",
+        "PostWasUpated",
+        "PostWasDeleted",
+        "CategoryWasCreated",
+        "CategoryWasUpated",
+        "CategoryWasDeleted",
+        
+    ];
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+       foreach ($this->events as $event) {
+            
+            $dbEvent = event::where("module_name", "Iblog")
+            ->where("name", $event)
+            ->first();
+            
+            if(empty($dbEvent)){
+                event::create([
+                    "module_name" => "Iblog",
+                    "name" => $event
+                ]);
+
+            }
+        }
+        
+    }
+}
