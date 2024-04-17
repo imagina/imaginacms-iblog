fa681296b40ed514ed772186f6fb9291235d79e7
Cristian Marin <marin101998@gmail.com>
2024-04-17 13:50:35 -0500

    +update | v10.x 17-04-24 last version v8.x

--- Resources/lang/en/cms.php
+++ Resources/lang/en/cms.php
@@ -1,28 +1,29 @@
 <?php
 
 return [
-    'sidebar' => [
-        'adminGroup' => 'Blog',
-        'adminPosts' => 'Blog Posts',
-        'adminCategories' => 'Blog Categories',
-        'adminPostsInternal' => 'View Blog Posts',
-    ],
-    'newPost' => 'New Post',
-    'updatePost' => 'Update Post',
-    'newCategory' => 'New Category',
-    'updateCategory' => 'Update Category',
-    'post' => 'Post',
-    'quickCard' => [
-        'lastPosts' => 'Blog - Last Posts',
-    ],
-    'form' => [
-        'showInMenu' => 'Show in Menu',
-        'featured' => 'Featured',
-        'sortOrder' => 'Sort Order',
-        'availableDate' => 'Available Date',
-    ],
-    'documentation' => [
-        'posts' => 'The articles are the content published on the Blog pages on your WEB site, these articles are
+  "sidebar" => [
+    "adminGroup" => "Blog",
+    "adminPosts" => "Blog Posts",
+    "adminCategories" => "Blog Categories",
+    "adminPostsInternal" => "View Blog Posts"
+  ],
+  "newPost" => "New Post",
+  "updatePost" => "Update Post",
+  "newCategory" => "New Category",
+  "updateCategory" => "Update Category",
+  "post" => "Post",
+  "category" => "Category",
+  "quickCard" => [
+    "lastPosts" => "Blog - Last Posts"
+  ],
+  "form" => [
+    "showInMenu" => "Show in Menu",
+    "featured" => "Featured",
+    "sortOrder" => "Sort Order",
+    "availableDate" => "Available Date"
+  ],
+  "documentation" => [
+    "posts" => 'The articles are the content published on the Blog pages on your WEB site, these articles are
      important for your site because they help to keep your site constantly updated. You may edit them
      adding images, videos or external links, as well as being able to modify the content of your texts.',
         'categories' => 'It is possible to create categories in your Blog, with a name, description and organize them in tree mode
