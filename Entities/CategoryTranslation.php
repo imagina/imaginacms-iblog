c0b89e0ef10908ef58e448eaa213c28b6e6e1813
msolanogithub <msolano@imaginacolombia.com>
2024-06-07 18:13:28 -0500

    removed mutator to meta fields of category entity it was affecting the translation results

--- Entities/CategoryTranslation.php
+++ Entities/CategoryTranslation.php
@@ -7,48 +7,36 @@ use Cviebrock\EloquentSluggable\Sluggable;
 
 class CategoryTranslation extends Model
 {
-    use Sluggable;
+  use Sluggable;
 
-    public $timestamps = false;
-    protected $table = 'iblog__category_translations';
-    protected $fillable = ['title', 'description', 'status', 'slug', 'meta_title', 'meta_description', 'meta_keywords', 'translatable_options'];
+  public $timestamps = false;
+  protected $table = 'iblog__category_translations';
+  protected $fillable = ['title', 'description', 'status', 'slug', 'meta_title', 'meta_description', 'meta_keywords', 'translatable_options'];
 
-    protected $casts = [
-        'translatable_options' => 'array'
+  protected $casts = [
+    'translatable_options' => 'array'
+  ];
+
+  /**
+   * Return the sluggable configuration array for this model.
+   *
+   * @return array
+   */
+  public function sluggable()
+  {
+    return [
+      'slug' => [
+        'source' => 'title'
+      ]
     ];
+  }
 
-    /**
-     * Return the sluggable configuration array for this model.
-     *
-     * @return array
-     */
-       public function sluggable()
-       {
-           return [
-               'slug' => [
-                   'source' => 'title'
-               ]
-        ];
-       }
-    public function getMetaDescriptionAttribute(){
+  public function getTranslatableOptionAttribute($value)
+  {
 
-        return $this->meta_description ?? substr(strip_tags($this->description??''),0,150);
-    }
-    public function getTranslatableOptionAttribute($value) {
-
-        $options=json_decode($value);
-        return $options;
+    $options = json_decode($value);
+    return $options;
 
 
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getMetaTitleAttribute(){
-
-        return $this->meta_title ?? $this->title;
-    }
-
-
+  }
 }
