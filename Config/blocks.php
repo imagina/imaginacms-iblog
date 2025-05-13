c9e5b51de4542b7f769aaff356e5c5cd3aa0b641
josuebedoya348 <145929750+josuebedoya@users.noreply.github.com>
2025-05-13 10:42:13 -0500

    Backend | Initialized configs $params for the Timeline component. (#93)

--- Config/blocks.php
+++ Config/blocks.php
@@ -13,10 +13,227 @@ return [
     "value" => "Modules\Iblog\Repositories\PostRepository"
    ],
   ],
+   "contentFields" => [
+     "title" => [
+       "name" => "title",
+       "type" => "input",
+       "columns" => "col-12",
+       "isTranslatable" => true,
+       "props" => [
+         "label" => "Titulo",
+       ]
+     ],
+     "subtitle" => [
+       "name" => "subtitle",
+       "type" => "html",
+       "columns" => "col-12",
+       "isTranslatable" => true,
+       "props" => [
+         "label" => "Subtitulo",
+       ]
+     ],
+   ],
   "childBlocks" => [
    "itemComponentAttributes" => "isite::item-list",
   ],
   "attributes" => [
+    "Titulos" => [
+      "title" => "Texto (Titulo y Subtitulo)",
+      "fields" => [
+        "textPosition" => [
+          "name" => "textPosition",
+          "value" => "2",
+          "type" => "select",
+          "columns" => "col-12",
+          "props" => [
+            "label" => "Posición",
+            "options" => [
+              ["label" => "Solo título", "value" => "1"],
+              ["label" => "Título con descripción abajo", "value" => "2"],
+              ["label" => "Título abajo con descripción arriba", "value" => "3"]
+            ]
+          ]
+        ],
+        "textAlign" => [
+          "name" => "textAlign",
+          "value" => "text-left",
+          "type" => "select",
+          "props" => [
+            "label" => "Alineación",
+            "options" => $vAttributes["align"]
+          ]
+        ],
+        "titleSize" => [
+          "name" => "titleSize",
+          "type" => "input",
+          "props" => [
+            "label" => "Tamaño Fuente (Titulo)",
+            "type" => "number"
+          ]
+        ],
+        "titleTransform" => [
+          "name" => "titleTransform",
+          "type" => "select",
+          "props" => [
+            "label" => "Transformar (Titulo)",
+            "options" => $vAttributes["textTransform"]
+          ]
+        ],
+        "titleColor" => [
+          "name" => "titleColor",
+          "type" => "select",
+          "props" => [
+            "label" => "Color (Titulo)",
+            "options" => $vAttributes["textColors"]
+          ]
+        ],
+        "titleMarginT" => [
+          "name" => "titleMarginT",
+          "value" => "mt-0",
+          "type" => "select",
+          "props" => [
+            "label" => "Margen superior (Titulo)",
+            "options" => $vAttributes["marginT"]
+          ]
+        ],
+        "titleMarginB" => [
+          "name" => "titleMarginB",
+          "value" => "mb-0",
+          "type" => "select",
+          "props" => [
+            "label" => "Margen inferior (Titulo)",
+            "options" => $vAttributes["marginB"]
+          ]
+        ],
+        "titleWeight" => [
+          "name" => "titleWeight",
+          "value" => "font-weight-normal",
+          "type" => "select",
+          "props" => [
+            "label" => "Negrita (Titulo)",
+            "options" => $vAttributes["textWeight"]
+          ]
+        ],
+        "titleLetterSpacing" => [
+          "name" => "titleLetterSpacing",
+          "type" => "input",
+          "props" => [
+            "label" => "Espacio entre letras (Titulo)",
+            "type" => "number"
+          ]
+        ],
+        "titleVineta" => [
+          "name" => "titleVineta",
+          "type" => "input",
+          "props" => [
+            "label" => "Icon (Titulo)"
+          ]
+        ],
+        "titleVinetaColor" => [
+          "name" => "titleVinetaColor",
+          "type" => "select",
+          "props" => [
+            "label" => "Color icon (Titulo)",
+            "options" => $vAttributes["textColors"]
+          ]
+        ],
+        "titleClasses" => [
+          "name" => "titleClasses",
+          "value" => "",
+          "type" => "input",
+          "columns" => "col-12",
+          "props" => [
+            "label" => "Clases (Titulo)",
+          ]
+        ],
+        "subtitleSize" => [
+          "name" => "subtitleSize",
+          "type" => "input",
+          "props" => [
+            "label" => "Tamaño Fuente (Subtitulo)",
+            "type" => "number"
+          ]
+        ],
+        "subtitleColor" => [
+          "name" => "subtitleColor",
+          "type" => "select",
+          "props" => [
+            "label" => "Color (Subtitulo)",
+            "options" => $vAttributes["textColors"]
+          ]
+        ],
+        "subtitleMarginT" => [
+          "name" => "subtitleMarginT",
+          "value" => "mt-0",
+          "type" => "select",
+          "props" => [
+            "label" => "Margen superior (Subtitulo)",
+            "options" => $vAttributes["marginT"]
+          ]
+        ],
+        "subtitleMarginB" => [
+          "name" => "subtitleMarginB",
+          "value" => "mb-0",
+          "type" => "select",
+          "props" => [
+            "label" => "Margen inferior (Subtitulo)",
+            "options" => $vAttributes["marginB"]
+          ]
+        ],
+        "subtitleTransform" => [
+          "name" => "subtitleTransform",
+          "type" => "select",
+          "props" => [
+            "label" => "Transformar (Subtitulo)",
+            "options" => $vAttributes["textTransform"]
+          ]
+        ],
+        "subtitleWeight" => [
+          "name" => "subtitleWeight",
+          "type" => "select",
+          "props" => [
+            "label" => "Negrita (Subtitulo)",
+            "options" => $vAttributes["textWeight"]
+          ]
+        ],
+        "subtitleLetterSpacing" => [
+          "name" => "subtitleLetterSpacing",
+          "type" => "input",
+          "props" => [
+            "label" => "Espacio entre letras (Subtitulo)",
+            "type" => "number"
+          ]
+        ],
+        "subtitleClasses" => [
+          "name" => "subtitleClasses",
+          "value" => "",
+          "type" => "input",
+          "columns" => "col-12",
+          "props" => [
+            "label" => "Clases (Subtitulo)",
+          ]
+        ],
+        "withLineTitle" => [
+          "name" => "withLineTitle",
+          "value" => "0",
+          "type" => "select",
+          "columns" => "col-12",
+          "props" => [
+            "label" => "Linea",
+            "options" => $vAttributes["titleLine"]
+          ]
+        ],
+        "lineTitleConfig" => [
+          "name" => "lineTitleConfig",
+          "value" => ['background' => 'var(--primary)','height' => '2px','width' => '10%','margin' => '0 auto'],
+          "type" => "json",
+          "columns" => "col-12",
+          "props" => [
+            "label" => "Configuración de Línea",
+          ]
+        ],
+      ]
+    ],
    "general" => [
     "title" => "General",
     "fields" => [
@@ -224,67 +441,87 @@ return [
      ],
     ],
    ],
-   "date" => [
-    "title" => "Fecha",
+   "contentLabel" => [
+    "title" => "Contenido Etiqueta",
     "fields" => [
-     "withDate" => [
-      "name" => "withDate",
-      "value" => "1",
+     "withContentLabel" => [
+      "name" => "withContentLabel",
+      "value" => "0",
       "type" => "select",
       "props" => [
-       "label" => "Mostrar Fecha",
+       "label" => "Mostrar Label",
        "options" => $vAttributes[ "validation" ],
       ],
      ],
-     "colorDate" => [
-      "name" => "colorDate",
+      "contentLabel" => [
+        "name" => "contentLabel",
+        "type" => "input",
+        "props" => [
+          "label" => "Campo/s a mostrar en el label",
+          "help" => [
+            "description" => "Valores Separados por coma(,)",
+          ]
+        ]
+      ],
+     "colorContentLabel" => [
+      "name" => "colorContentLabel",
       "type" => "input",
       "props" => [
-       "label" => "Color de la Fecha",
+       "label" => "Color",
       ],
      ],
-     "bgDate" => [
-      "name" => "bgDate",
+     "bgContentLabel" => [
+      "name" => "bgContentLabel",
       "type" => "input",
       "props" => [
-       "label" => "Fondo de la Fecha",
+       "label" => "Color Fondo",
        "help" => [
         "description" => "Solo valores hexadecimales o variables (#000, var(), White...",
        ]
       ]
      ],
-     "sizeDate" => [
-      "name" => "sizeDate",
+     "sizeContentLabel" => [
+      "name" => "sizeContentLabel",
       "type" => "input",
       "props" => [
        "type" => "number",
-       "label" => "Tamaño de la Fecha"
+       "label" => "Tamaño "
       ],
      ],
-     "paddingDate" => [
-      "name" => "paddingDate",
+     "paddingContentLabel" => [
+      "name" => "paddingContentLabel",
       "type" => "input",
       "props" => [
        "type" => "number",
        "label" => "Espaciado"
       ],
      ],
-     "marginDate" => [
-      "name" => "marginDate",
+     "marginContentLabel" => [
+      "name" => "marginContentLabel",
       "type" => "input",
       "props" => [
        "type" => "text",
        "label" => "Margen"
       ],
      ],
-     "radiusDate" => [
-      "name" => "radiusDate",
+     "radiusContentLabel" => [
+      "name" => "radiusContentLabel",
       "type" => "input",
       "props" => [
        "type" => "number",
        "label" => "Redondeado del borde"
       ],
      ],
+      "formatDate" => [
+        "name" => "formatDate",
+        "value" => "d \\d\\e M",
+        "type" => "select",
+        "columns"=> "col-12",
+        "props" => [
+          "label" => "Formato",
+          "options" => $vAttributes["formatCreatedDate"]
+        ]
+      ],
     ],
    ],
   ],
